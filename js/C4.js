ece29ebf9453182ae6f219767999a6c088fe4dfb
Iula79 <giulialake@gmail.com>
2015-10-15 17:35:01 -0400

    final setup

--- js/C4.js
+++ js/C4.js
@@ -1,32 +1,56 @@
 $(document).ready(function() {
     console.log('loaded');
-
+    //creating a board in js
+    var newBoard = new MyBoard();
+    newBoard.makeBoard();
+    //creating a board in html
     var board = $('.board');
-    for (var i = 0; i < 6; i++) {
-        var myRow = $('<div>');
-        console.log(myRow);
-        myRow.addClass("row");
-        identifier = "R" + i;
-        console.log(identifier);
-        myRow.attr("id", identifier);
+    //adding an event listener to each cell
+    board.on("click", ".cell", function() {
+        var index = this.id.split(' ');
+        var cell = newBoard.board[index[0]][index[1]];
+        console.log(cell.playable);
+        console.log(cell);
+    });
 
-        for (var j = 0; j < 7; j++) {
-            var myCell = $('<div>');
-            console.log(myCell);
-            myCell.addClass("cell");
-            identifier = "C" + j;
-            console.log(identifier);
-            myCell.attr("id", identifier);
-            myRow.append(myCell);
-        }
-        board.append(myRow);
-    }
 });
 
 
 
+var Cell = function() {
+    this.value = null;
+    this.playable = true;
+    this.setValue = function() {
+        this.playable = false;
+        this.value = player;
+    };
+};
 
-/*var myh1 = $('<h1>');
-myh1.text("Connect Four");
-body = $('body');
-body.append(myh1);*/
+var MyBoard = function() {
+    this.board = [];
+    this.makeBoard = function() {
+        for (var i = 0; i < 6; i++) {
+            //creating the rows in html
+            var myRow = $('<div>');
+            console.log(myRow);
+            //adding the class to the rows
+            myRow.addClass("row");
+            //creating the rows in js
+            var row = [];
+            for (var j = 0; j < 7; j++) {
+                //adding the cells to the row
+                row.push(new Cell());
+                //creating the cells in html
+                var myCell = $('<div>');
+                myCell.addClass("cell");
+                var identifier = i + ' ' + j;
+                myCell.attr("id", identifier);
+                //attach cell divs to row divs
+                myRow.append(myCell);
+            }
+            //adding the rows to the board
+            this.board.push(row);
+            $('.board').append(myRow);
+        }
+    };
+};
