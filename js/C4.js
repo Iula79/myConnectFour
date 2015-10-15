2e74e61b14d0af1c2c9b665c0a1790e73fd8763b
Iula79 <giulialake@gmail.com>
2015-10-15 14:18:32 -0400

    initial styling

--- js/C4.js
+++ js/C4.js
@@ -1,6 +1,32 @@
 $(document).ready(function() {
-console.log('loaded');
-boardDivs = $('div*42>div');
-body = $('body');
-body.append(boardDivs);
+    console.log('loaded');
+
+    var board = $('.board');
+    for (var i = 0; i < 6; i++) {
+        var myRow = $('<div>');
+        console.log(myRow);
+        myRow.addClass("row");
+        identifier = "R" + i;
+        console.log(identifier);
+        myRow.attr("id", identifier);
+
+        for (var j = 0; j < 7; j++) {
+            var myCell = $('<div>');
+            console.log(myCell);
+            myCell.addClass("cell");
+            identifier = "C" + j;
+            console.log(identifier);
+            myCell.attr("id", identifier);
+            myRow.append(myCell);
+        }
+        board.append(myRow);
+    }
 });
+
+
+
+
+/*var myh1 = $('<h1>');
+myh1.text("Connect Four");
+body = $('body');
+body.append(myh1);*/
